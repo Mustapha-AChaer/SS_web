6bd9c973df7bc7903c25cb2422564a987520f0eb
Sebastian <madridx2010@gmail.com>
2022-01-28 12:22:36 -0500

    Update index.js

--- src/components/mint-bar/index.js
+++ src/components/mint-bar/index.js
@@ -23,7 +23,7 @@ const MintBar = props => {
         {/*<h1 className="title is-4 has-text-white mb-2">{totalSupply} / 5000</h1>
             <progress className="progress is-info" value={(totalSupply / 5000) * 100} max="100" /> */}
             <h1 className="title is-4 has-text-white mb-2">Genesis Mint: 1000 / 1000</h1>
-            <progress className="progress is-info" value={(1000 / 5000) * 100} max="100" /> 
+            <progress className="progress is-info" value={(1000 / 1000) * 100} max="100" /> 
         </Fragment>
     );
 };
